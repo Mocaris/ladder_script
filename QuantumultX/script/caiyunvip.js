f21147dbca7a4038e7742729793fd0083c8c0bc4
Mocaris <imocaris@outlook.com>
2022-06-07 13:06:43 +0800

    Delete caiyunvip.js

--- QuantumultX/script/caiyunvip.js
+++
@@ -1,9 +0,0 @@
-// 彩云天气VIP 2022-05-12 16:26:33
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj["result"]["svip_expired_at"]=4070883661;
-obj["result"]["wt"]["vip"]["svip_expired_at"]=4070883661;
-body=JSON.stringify(obj);
-
-$done(body);
